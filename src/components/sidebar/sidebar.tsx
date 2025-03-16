7da60ee702adc6f1f0db57d297882e3ad8d2a3e2
Cuong3103 <cuongpdse173196@fpt.edu.vn>
2025-03-16 17:55:46 +0700

    feat/sidebar: setup sidebar

---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { MdLogout } from "react-icons/md";
+
+type SidebarProps = {
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+};
+
+export default function Sidebar({ activeTab, setActiveTab }: SidebarProps) {
+  const MENU_ITEMS = [
+    {
+      title: "Profile",
+      icon: () => (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-5 h-5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      title: "Logout",
+      icon: () => <MdLogout />,
+    },
+  ];
+
+  return (
+    <div className="relative flex h-screen w-full max-w-[20rem] flex-col bg-white p-4 text-gray-700 shadow-xl shadow-blue-gray-900/5">
+      <div className="flex p-4 my-2 relative">
+        <h5 className="block text-xl font-semibold text-blue-gray-900 ml-8">
+          Sidebar
+        </h5>
+      </div>
+
+      <nav className="flex flex-col gap-1 p-2 text-base font-normal text-blue-gray-700">
+        {MENU_ITEMS.map((item, index) => (
+          <div
+            key={index}
+            role="button"
+            onClick={() => setActiveTab(item.title)}
+            className={`flex items-center p-3 transition-all rounded-lg hover:bg-blue-gray-50 hover:text-blue-gray-900 cursor-pointer ${
+              activeTab === item.title ? "bg-black text-white" : ""
+            }`}
+          >
+            <div
+              className={`grid mr-4 text-${activeTab === item.title ? "white" : "black"} place-items-center ${activeTab === item.title ? "" : ""}`}
+            >
+              {item.icon()}
+            </div>
+            <span>{item.title}</span>
+          </div>
+        ))}
+      </nav>
+    </div>
+  );
+}
